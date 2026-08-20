986a80d43e2f9160bef15b9bfee4f3a280092e8c
asp <asp@aptechnn2.edu>
2026-08-20 17:56:13 +0500

    test

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:3000/login", {
+    const response = await fetch("https://fitness-backend-31hb.onrender.com/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -17,7 +17,7 @@ function Register() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:3000/register", {
+    const response = await fetch("https://fitness-backend-31hb.onrender.com/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
